4633191407eece8e68e927d85d802587733c7424
vaibhav484848 <113878147+vaibhav484848@users.noreply.github.com>
2025-01-09 16:01:14 +0530

    Time: 6 ms (5.13%) | Memory: 43.1 MB (10.26%) - LeetSync

--- 3297-minimum-time-to-revert-word-to-initial-state-i/minimum-time-to-revert-word-to-initial-state-i.java
+++ 3297-minimum-time-to-revert-word-to-initial-state-i/minimum-time-to-revert-word-to-initial-state-i.java
@@ -1,16 +1,40 @@
 class Solution {
+    int[] kmp(String s){
+        int n=s.length() ;
+        int[] lcs=new int[n];
+
+        for(int i=1;i<n;i++){
+            
+            int prevIdx=lcs[i-1];
+
+            while(prevIdx>0 && s.charAt(i)!=s.charAt(prevIdx)){
+                prevIdx=lcs[prevIdx-1];
+            }
+            lcs[i]=prevIdx+ (s.charAt(i)==s.charAt(prevIdx)?1:0);
+        }
+
+        return lcs;
+    }
     public int minimumTimeToInitialState(String word, int k) {
         int n=word.length();
         if(k==n) return 1;
         String temp=word;
         int count=0;
 
+        
+
+
         while(true){
 
             temp=temp.substring(k);
             count++;
             
-            if(word.startsWith(temp)) return count;
+            // if(word.startsWith(temp)) return count;
+
+            int[] kmp1=kmp(temp+"#"+word.substring(0,temp.length()));
+            if(kmp1[kmp1.length-1]==temp.length()) return count;
+
+
 
             if(temp.length()<=k) {
                 count++;
