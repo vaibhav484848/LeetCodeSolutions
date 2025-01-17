146485ad215ec0a61d92dbaf0f8ecd787eff42e3
vaibhav484848 <113878147+vaibhav484848@users.noreply.github.com>
2025-01-17 10:10:01 +0530

    Time: 2 ms (100.00%) | Memory: 62.5 MB (32.94%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
@@ -3,32 +3,12 @@ class Solution {
 
         int n=derived.length;
 
+        int xorValue=0;
 
-        int prevEle=0;
-        int prevXor=derived[0];
-
-        for(int i=1;i<n;i++){
-            if(prevXor==1){
-                if(prevEle==0){
-                    prevEle=1;
-                }
-                else{
-                    prevEle=0;
-                }
-
-            }
-            prevXor=derived[i];
-            
+        for(int i=0;i<n;i++){
+            xorValue^=derived[i];
         }
-
-        if(prevEle==0){
-            if(derived[n-1]==0) return true;
-            return false;
-        }
-        else{
-            if(derived[n-1]==1) return true;
-            return false;
-        }
+        return xorValue==0;
 
         
     }
