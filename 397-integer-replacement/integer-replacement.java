303fef82a40f70e684c8c515593bb6b69de26e8a
vaibhav484848 <113878147+vaibhav484848@users.noreply.github.com>
2025-01-20 12:20:05 +0530

    Time: 1 ms (58.83%) | Memory: 40.6 MB (29.79%) - LeetSync

---
+++ 397-integer-replacement/integer-replacement.java
@@ -0,0 +1,37 @@
+class Solution {
+    // Long[] dp;
+    HashMap<Long,Integer> map;
+
+    int ans(long n){
+        if(n==1 ) return 0;
+        
+
+        int pick1=Integer.MAX_VALUE;
+
+        if(map.containsKey(n)) return map.get(n);
+
+        if(n%2==0){
+
+            pick1=1+ans(n/2);
+        }
+        else{
+            pick1=1+ans(n-1);
+            pick1=Math.min(pick1,1+ans(n+1));
+        }
+
+        map.put((long)n,pick1);
+
+        return pick1;
+    }
+    public int integerReplacement(int n) {
+
+        int count=0;
+        map=new HashMap<>();
+        return (int)ans(n);
+
+        
+
+
+        
+    }
+}
