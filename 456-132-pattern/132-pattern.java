aeacbd5f7d6dc91a99bdf335527c83d31c6d26f4
vaibhav484848 <113878147+vaibhav484848@users.noreply.github.com>
2025-02-05 15:06:43 +0530

    Time: 72 ms (5.02%) | Memory: 89.3 MB (5.85%) - LeetSync

---
+++ 456-132-pattern/132-pattern.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean find132pattern(int[] nums) {
+
+        int n=nums.length;
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        int[] pge=new int[n];
+
+        int[] min=new int[n];
+        min[0]=Integer.MAX_VALUE;
+        map.put(nums[0],min[0]);
+
+        for(int i=1;i<n;i++){
+            min[i]=Math.min(min[i-1],nums[i-1]);
+            map.put(i,min[i]);
+        }
+
+        Stack<Integer> st=new Stack<>();
+
+        for(int i=0;i<n;i++){
+            int ele=nums[i];
+            while(st.size()>0 && nums[st.peek()]<=ele){
+                st.pop();
+            }
+            if(st.size()==0) pge[i]=Integer.MIN_VALUE;
+            else pge[i]=st.peek();
+
+            st.push(i);
+        }
+
+
+        // System.out.println(Arrays.toString(pge));
+
+        for(int i=2;i<n;i++){
+            if(pge[i]!=Integer.MIN_VALUE && map.getOrDefault(pge[i],Integer.MAX_VALUE)<nums[i]) {
+                System.out.println(i);
+                return true;
+            }
+        }
+
+        return false;
+
+
+        
+    }
+}
