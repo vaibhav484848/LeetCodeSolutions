a5249fd01f8ea123fc4345fa9035d8bcf05abd45
vaibhav484848 <113878147+vaibhav484848@users.noreply.github.com>
2025-02-14 11:13:36 +0530

    Time: 17 ms (33.70%) | Memory: 69 MB (53.70%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.java
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.java
@@ -1,27 +1,48 @@
 class ProductOfNumbers {
 
     List<Integer> pre;
+    int zeroIdx;
 
     public ProductOfNumbers() {
         pre=new ArrayList<>();
+        zeroIdx=-1;
         
     }
     
     public void add(int num) {
+        int size=pre.size();
+        if(num==0) zeroIdx=size;
+       
+        if(size==0) pre.add(num);
         
-        pre.add(num);
+        else{
+            int prev=pre.get(size-1);
+            if(prev==0)prev=1;
+            pre.add(prev*num);
+        }
+        
         
     }
     
     public int getProduct(int k) {
-        int i=0;
-        int j=pre.size()-1;
-        int pro=1;
-        while(i<k){
-            pro*=pre.get(j--);
-            i++;
+
+        int size=pre.size();
+        int kStart=size-k;
+
+        if(zeroIdx>=kStart) return 0;
+        else{
+
+            int nemo=pre.get(size-1);
+            int deno=1;
+            if(kStart>=1){
+                deno=pre.get(kStart-1);
+                
+                if(deno==0) deno=1;
+                
+            }
+            return nemo/deno;
         }
-        return pro;
+        
 
         
     }
