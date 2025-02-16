d2bef7e04d3de5416a1bcfc2400879ae7d77ff50
vaibhav484848 <113878147+vaibhav484848@users.noreply.github.com>
2025-02-16 14:55:41 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (67.88%) - LeetSync

---
+++ 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.java
@@ -0,0 +1,67 @@
+class Solution {
+
+    int count;
+    int[] fans;
+    boolean flag;
+
+    boolean helper(int n,boolean[] visited,int idx,int[]ans){
+        if(idx==count ) {
+            
+            return true;
+        }
+        if(ans[idx]!=0) {
+            
+            return helper(n,visited,idx+1,ans);
+        }
+        
+        
+        for(int i=n;i>=1;i--)
+        {
+
+             
+
+            if(visited[i]) continue;
+            if(i==1){
+                ans[idx]=1;
+                visited[i]=true;
+                if(helper(n,visited,idx+1,ans)) return true;
+                ans[idx]=0;
+                visited[i]=false;
+               
+            }
+            else{
+
+                if(idx+i>=ans.length || (ans[idx+i]!=0)) continue;
+                ans[idx]=i;
+                
+                ans[idx+i]=i;
+                visited[i]=true;
+                if(helper(n,visited,idx+1,ans)) return true;
+                ans[idx]=0;
+                ans[idx+i]=0;
+                visited[i]=false;
+
+               
+            }
+            
+            
+
+        }
+        return false;
+
+    }
+    public int[] constructDistancedSequence(int n) {
+
+        boolean[] visited=new boolean[n+1];
+
+        
+        count=2*n-1;
+
+        int[] ans=new int[count];
+
+        helper(n,visited,0,ans);
+
+        return ans;
+        
+    }
+}
