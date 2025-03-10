d0e102de36f5ded986838f63bd7bc78050e9fea0
vaibhav484848 <113878147+vaibhav484848@users.noreply.github.com>
2025-03-10 13:41:29 +0530

    Time: 798 ms (5.13%) | Memory: 46.2 MB (69.23%) - LeetSync

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
@@ -16,59 +16,40 @@ class Solution {
 
         int conso=0;
 
-        
 
         while(r<n){
             char ch=word.charAt(r);
-            map.put(ch,map.getOrDefault(ch,0)+1);
-            if(ch!='a' && ch!='e' && ch!='i' && ch!='o' && ch!='u') conso++;
-            else{
-                // lastPos[ch-'a']=r;
-                if(ch=='a') lastPos[0]=r;
-                if(ch=='e') lastPos[1]=r;
-                if(ch=='i') lastPos[2]=r;
-                if(ch=='o') lastPos[3]=r;
-                if(ch=='u') lastPos[4]=r;
-
-            }
-            // System.out.println(conso);
-
             
-           
-            while(conso>k){
-                char chL=word.charAt(l);
-                if(map.get(chL)==1) map.remove(chL);
-                else map.put(chL,map.get(chL)-1);
+            if(ch!='a' && ch!='e' && ch!='i' && ch!='o' && ch!='u') conso++;
+            else map.put(ch,map.getOrDefault(ch,0)+1);
 
+            while(vowels(map) && conso>=k){
+                count+=(n-r);
+                char chL=word.charAt(l);
                 if(chL!='a' && chL!='e' && chL!='i' && chL!='o' && chL!='u') conso--;
+                else{
+                    if(map.get(chL)==1) map.remove(chL);
+                    else map.put(chL,map.get(chL)-1);
+                }
                 l++;
                 
             }
-            int minPos = Integer.MAX_VALUE;
-            for (int pos : lastPos) {
-                minPos = Math.min(minPos, pos);
-            }
-             if(vowels(map) && conso<=k) {
-                count+=(minPos-l+1);
-                
-                // count+=(r-l+1);   
-            }
-                
-            
             r++;
         }
         return count;
     }
     public long countOfSubstrings(String word, int k) {
 
-        // long fans=ans(word,k)-ans(word,k-1);
+        
         long ans1=ans(word,k);
-        long ans2=((k>=1)?ans(word,k-1):0);
+        long ans2=ans(word,k+1);
 
         // System.out.println(ans1+" "+ans2);
 
+        
+
         return ans1-ans2;
         
-        // return fans;
+        
     }
 }
