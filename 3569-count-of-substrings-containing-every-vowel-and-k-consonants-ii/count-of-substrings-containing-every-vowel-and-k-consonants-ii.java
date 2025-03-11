4caa31bd259eb60e652e6a4b6ef3705d7824580e
vaibhav484848 <113878147+vaibhav484848@users.noreply.github.com>
2025-03-11 15:51:56 +0530

    Time: 171 ms (74.36%) | Memory: 49.6 MB (18.80%) - LeetSync

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
@@ -1,62 +1,63 @@
 class Solution {
-    
-    Set<Character> set =new HashSet<>();
-    boolean vowels(Map<Character,Integer> map){
-        if(map.containsKey('a') && map.containsKey('e') && map.containsKey('i') && map.containsKey('o') && map.containsKey('u') ) return true;
-        return false;
-    }
-    long ans(String word,int k){
+    public long countOfSubstrings(String word, int k) {
         int n=word.length();
+        int[] suff=new int[n];
+        suff[n-1]=n;
+        for(int i=n-2;i>=0;i--){
+            char ch=word.charAt(i+1);
+            if(ch!='a' && ch!='e' && ch!='i' && ch!='o' && ch!='u'){
+                suff[i]=i+1;
+            }
+            else suff[i]=suff[i+1];
+        }
+        // System.out.println(Arrays.toString(suff));
 
-        Map<Character,Integer> map=new HashMap<>();
-
-        int l=0;
-        int r=0;
-
-        long count=0;
-
+        int l=0,r=0;
+        long countValidSub=0;
         int conso=0;
-
+        Map<Character,Integer>map=new HashMap<>();
 
         while(r<n){
             char ch=word.charAt(r);
-            
-            if(!set.contains(ch)) conso++;
-            else map.put(ch,map.getOrDefault(ch,0)+1);
+            if(ch!='a' && ch!='e' && ch!='i' && ch!='o' && ch!='u'){
+                conso++;
+            }
+            else{
+                map.put(ch,map.getOrDefault(ch,0)+1);
+            }
 
-            while(vowels(map) && conso>=k){
-                count+=(n-r);
+            while(map.size()==5 && conso==k){
+                int nextConsoIdx=suff[r];
+                countValidSub+=(nextConsoIdx-r);
                 char chL=word.charAt(l);
-                // if(chL!='a' && chL!='e' && chL!='i' && chL!='o' && chL!='u') conso--;
-                if(!set.contains(chL))conso--;
+                if(chL!='a' && chL!='e' && chL!='i' && chL!='o' && chL!='u'){
+                    conso--;
+                }
                 else{
                     if(map.get(chL)==1) map.remove(chL);
                     else map.put(chL,map.get(chL)-1);
                 }
-                l++;
                 
+                l++;
             }
+
+            while(conso>k){
+                char chL=word.charAt(l);
+                if(chL!='a' && chL!='e' && chL!='i' && chL!='o' && chL!='u'){
+                    conso--;
+                }
+                else{
+                    if(map.get(chL)==1) map.remove(chL);
+                    else map.put(chL,map.get(chL)-1);
+                }
+                
+                l++;
+            }
+
             r++;
         }
-        return count;
-    }
-    public long countOfSubstrings(String word, int k) {
 
-        set.add('a');
-        set.add('e');
-        set.add('i');
-        set.add('o');
-        set.add('u');
-
-        long ans1=ans(word,k);
-        long ans2=ans(word,k+1);
-
-        // System.out.println(ans1+" "+ans2);
-
-        
-
-        return ans1-ans2;
-        
+        return countValidSub;
         
     }
 }
