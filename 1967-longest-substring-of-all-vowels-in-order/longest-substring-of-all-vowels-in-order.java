0fa0142ac188c7a872fe1a431e0d8ae69e82833e
vaibhav484848 <113878147+vaibhav484848@users.noreply.github.com>
2025-03-11 20:04:52 +0530

    Time: 100 ms (5.14%) | Memory: 55.1 MB (5.14%) - LeetSync

--- 1967-longest-substring-of-all-vowels-in-order/longest-substring-of-all-vowels-in-order.java
+++ 1967-longest-substring-of-all-vowels-in-order/longest-substring-of-all-vowels-in-order.java
@@ -1,109 +1,28 @@
 class Solution {
     public int longestBeautifulSubstring(String word) {
 
-        Map<Character,Integer> map=new HashMap<>();
+        Map<Character,Integer> map = new HashMap<>();
 
-        int l=0,r=0;
-        int n=word.length();
+        int l=0,r=1,n=word.length();
         int count=0;
+        map.put(word.charAt(0),1);
 
         while(r<n){
-            char ch=word.charAt(r);
-            if(ch=='a'){
-                if((map.containsKey('a') && map.size()==1) || map.size()==0){
-                    map.put(ch,map.getOrDefault(ch,0)+1);
-                }
-                else{
-                    l=r;
-                    map.clear();
-                    map.put(ch,map.getOrDefault(ch,0)+1);
-                    r++;
-                    continue;
-                }
+            char present=word.charAt(r);
+            char previous=word.charAt(r-1);
+            if(present<previous){
+                l=r;
+                map.clear();
+                map.put(present,1);
                 
             }
-            else if(ch=='e' ) {
-                if(!map.containsKey('a')) {
-                    l=r+1;
-                    map.clear();
-                    r++;
-                    continue;
-                }
-                if(map.containsKey('e')&& map.size()==2){
-                    map.put(ch,map.getOrDefault(ch,0)+1);
-                }
-                else if(map.size()==1) map.put(ch,1);
-                else{
-                    l=r+1;
-                    map.clear();
-                    r++;
-                    continue;
-                }
-            }
-            else if(ch=='i'){
-                if(!map.containsKey('a') || !map.containsKey('e')){
-                    l=r+1;
-                    map.clear();
-                    r++;
-                    continue;
-                }
-                if(map.containsKey('i')&& map.size()==3){
-                    map.put(ch,map.getOrDefault(ch,0)+1);
-                }
-                else if(map.size()==2) map.put(ch,1);
-                else{
-                    l=r+1;
-                    map.clear();
-                    r++;
-                    continue;
-                }
-
-            }
-            else if(ch=='o'){
-                if(!map.containsKey('a') || !map.containsKey('e') || !map.containsKey('i')){
-                    l=r+1;
-                    map.clear();
-                    r++;
-                    continue;
-                }
-                if(map.containsKey('o')&& map.size()==4){
-                    map.put(ch,map.getOrDefault(ch,0)+1);
-                }
-                else if(map.size()==3) map.put(ch,1);
-                else{
-                    l=r+1;
-                    map.clear();
-                    r++;
-                    continue;
-                }
-
-            }
             else{
-
-                if(map.containsKey('u')&& map.size()==5){
-                    map.put(ch,map.getOrDefault(ch,0)+1);
-                    count=Math.max(count,r-l+1);
-                    // System.out.println(r+" "+l);
-                   
+                map.put(present,map.getOrDefault(present,0)+1);
+                if(map.size()==5){
+                    count=Math.max(r-l+1,count);
                 }
-                else if(map.size()==4) {
-                    map.put(ch,1);
-                    count=Math.max(count,r-l+1);
-                    // System.out.println(r+" "+l);
-                    
-                }
-                else{
-                    l=r+1;
-                    map.clear();
-                    r++;
-                    continue;
-                }
-
-
             }
             r++;
-            
-            
         }
 
         return count;
