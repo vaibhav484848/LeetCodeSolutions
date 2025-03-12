d8378b484f06d81c24676fee12093c5587a666f1
vaibhav484848 <113878147+vaibhav484848@users.noreply.github.com>
2025-03-12 09:29:34 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (96.35%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,13 +1,52 @@
 class Solution {
-    public int maximumCount(int[] nums) {
-        int posCount=0;
-        int n=nums.length;
+    int lowerBound(int[] nums){
+        int lo=0,hi=nums.length-1;
+        int ans=-1;
 
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>0) posCount++;
-            if(nums[i]==0) n--;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(nums[mid]==0){
+                hi=mid-1;
+            }
+            else if(nums[mid]<0){
+                ans=mid;
+                lo=mid+1;
+            }
+            else{
+                hi=mid-1;
+            }
         }
-        return Math.max(posCount,n-posCount);
+        return ans;
+    }
+    int upperBound(int[] nums){
+        int lo=0,hi=nums.length-1;
+        int ans=-1;
+
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(nums[mid]==0)lo=mid+1;
+            if(nums[mid]>0){
+                ans=mid;
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
+            }
+        }
+        return ans;
+    }
+    public int maximumCount(int[] nums) {
+        int n=nums.length;
+        int lB=lowerBound(nums)+1;
+        
+        
+        int uB=upperBound(nums);
+        if(uB==-1) uB=0;
+        else{
+            uB=n-uB;
+        }
+
+        return Math.max(uB,lB);
         
     }
 }
