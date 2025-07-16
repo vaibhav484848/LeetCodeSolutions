b40d324e15955e9e3612a1b604806ce6425b1e55
Vaibhav Yadav <113878147+vaibhav484848@users.noreply.github.com>
2025-07-16 13:31:11 +0530

    Time: 11 ms (26.23%) | Memory: 62.2 MB (59.84%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.java
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.java
@@ -8,9 +8,6 @@ class Solution {
 
         int maxLen=1;
         for(int i=0;i<n;i++) nums[i]%=2;
-        // for(int i=0;i<n;i++){
-        //     System.out.print(nums[i]+" ");
-        // }
 
         int ovSame=0;
 
@@ -20,9 +17,7 @@ class Solution {
             if(nums[i]%2==0) ev++;
             else odd++;
         }
-        // System.out.println();
         maxLen=Math.max(ev,odd);
-        //  System.out.print(maxLen+" ");
 
         
 
@@ -43,27 +38,22 @@ class Solution {
         }
 
         maxLen=Math.max(diff,maxLen);
-        // System.out.println();
-        // System.out.print(maxLen+" ");
 
 
-        diff=1;
+        diff=0;
 
-        // for(int i=1;i<n;i++){
-        //     if(flag2 && nums[i]!=0){
-        //         flag2=!flag2;
-        //         diff++;
-        //     }
-        //     else if(!flag2 && nums[i]==0){
-        //         flag2=!flag2;
-        //         diff++;
-        //     }
-        // }
+        for(int i=1;i<n;i++){
+            if(flag2 && nums[i]!=0){
+                flag2=!flag2;
+                diff++;
+            }
+            else if(!flag2 && nums[i]==0){
+                flag2=!flag2;
+                diff++;
+            }
+        }
 
-        // maxLen=Math.max(diff,maxLen);
-
-        // System.out.println();
-        // System.out.print(maxLen+" ");
+        maxLen=Math.max(diff,maxLen);
         
         return maxLen;
         
