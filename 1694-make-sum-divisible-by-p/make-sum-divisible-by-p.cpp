cb86041e2ce7ec96ab2e9fa377a5c2b35825abbc
Vaibhav Yadav <113878147+vaibhav484848@users.noreply.github.com>
2025-11-30 22:29:24 +0530

    Time: 187 ms (8.26%) | Memory: 90.3 MB (25.99%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
@@ -12,55 +12,30 @@ public:
         if(tSum%p==0) return 0;
         int remO=(tSum%p);
 
-        cout<<remO<<" this is remO\n";
 
         long long sum=0;
 
-        int ans=INT_MAX;
+        int ans=n;
+
+        mp[0]=-1;
 
         for(int i=0;i<n;i++){
             sum=sum+nums[i];
 
             int rem=sum%p;
 
-            // cout<<rem<<" "<<i<<"\n";
-
-            if(rem>remO){
-                if(mp.find(rem-remO)!=mp.end() ){
-                    int idx=mp[rem-remO];
-                    ans=min(ans,i-idx);
-                }
-            }
-            else if(rem<remO){
-                // 2 we want 4  p=6. 8,4 curr - curr -(p-want )
-
-                if(mp.find(rem+p-remO)!=mp.end()){
-                    // cout<<"hello "<<i<<'\n';
-                    int idx=mp[rem+p-remO];
-                    ans=min(ans,i-idx);
-                }
-            }
-            else{
-                if(mp.find(0)!=mp.end()){
-                    int idx=mp[0];
-                    ans=min(ans,i-idx);
-                }
-                else
-                {  
-                    if(i+1!=n)
-                    ans=min(ans,i+1);
-                }
-            }
-
-            // if(mp.find(sum%p)==mp.end()){
-                mp[(sum%p)]=i;
-            // }
-
+            int toFd=(rem-remO+p)%p;
             
-            // cout<<ans<<"\n";
+            if(mp.find(toFd)!=mp.end()){
+                ans=min(ans,i-mp[toFd]);
+            }
+            
+            mp[rem]=i;
+
+        
             
         }
 
-        return ans==INT_MAX?-1:ans;
+        return ans==n?-1:ans;
     }
 };
