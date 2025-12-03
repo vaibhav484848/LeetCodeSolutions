5fe8682e94675a1aa79dea16ad8f19c00fe2e817
Vaibhav Yadav <113878147+vaibhav484848@users.noreply.github.com>
2025-12-03 20:51:58 +0530

    Time: 35 ms (77.68%) | Memory: 106 MB (33.27%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -4,46 +4,27 @@ public:
 
     //3 6 9
 
-    bool check(int mid,vector<int>& nums, int k){
-        long long medIdx=(mid-1)/2,sum=0;
-
-        int n=nums.size();
-
-        int windowL=mid-1;
-        for(int i=0;i<n;i++){
-            sum+=abs(nums[windowL]-nums[i]);
-            if(i>=mid){
-                int diff=nums[i]-nums[i-1];
-                sum+=(diff*(mid-1));
-                // sum-=(nums[medIdx-1]-nums[i-mid]);
-            }
-            if(i>=mid-1){
-                
-                // cout<<mid<<" "<<medIdx<<" "<<sum<<"\n";
-                if(sum<=k) return true;
-                sum-=(nums[windowL]-nums[i-(mid-1)]);
-                windowL++;
-            }
-        }
-        return false;
-    }
+    
     int maxFrequency(vector<int>& nums, int k) {
         sort(nums.begin(),nums.end());
 
-        int lo=1,hi=nums.size(),ans=0;
+        long long i=0,j=0,prev=0,ans=0,sum=0;
+        int n=nums.size();
 
-        while(lo<=hi){
-            int mid=lo+(hi-lo)/2;
+        while(j<n){
+            int diff=nums[j]-prev;
+            prev=nums[j];
+            sum+=(j-i)*diff;
 
-            if(check(mid,nums,k)){
-                ans=mid;
-                lo=mid+1;
+            while(sum>k){
+                sum-=(nums[j]-nums[i]);
+                i++;
             }
-            else{
-                hi=mid-1;
-            }
+            ans=max(ans,j-i+1);
+            j++;
         }
 
+        
         return ans;
     }
 };
