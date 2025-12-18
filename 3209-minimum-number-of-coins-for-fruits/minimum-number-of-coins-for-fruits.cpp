228afe69151608ff2436f3a84d4cf5b4d125580b
Vaibhav Yadav <113878147+vaibhav484848@users.noreply.github.com>
2025-12-18 20:12:24 +0530

    Time: 11 ms (63.11%) | Memory: 24.7 MB (46.31%) - LeetSync

--- 3209-minimum-number-of-coins-for-fruits/minimum-number-of-coins-for-fruits.cpp
+++ 3209-minimum-number-of-coins-for-fruits/minimum-number-of-coins-for-fruits.cpp
@@ -1,35 +1,51 @@
 class Solution {
 public:
-    int dp[1005];
+    // int dp[1005];
 
-    int rec(int idx,vector<int>& prices){
-        if(idx>=prices.size()){
-            return 0;
-        }
-        if( dp[idx]!=-1){
-            return dp[idx];
-        }
-        int currIdx=idx+1;
+    // int rec(int idx,vector<int>& prices){
+    //     if(idx>=prices.size()){
+    //         return 0;
+    //     }
+    //     if( dp[idx]!=-1){
+    //         return dp[idx];
+    //     }
+    //     int currIdx=idx+1;
 
-        int a=1e9;
+    //     int a=1e9;
 
-        for(int i=idx+1;i<=2*currIdx;i++){
-            a= min(a,prices[idx]+ rec(i,prices));
-        }
+    //     for(int i=idx+1;i<=2*currIdx;i++){
+    //         a= min(a,prices[idx]+ rec(i,prices));
+    //     }
 
-        return dp[idx]=a;
-    }
+    //     return dp[idx]=a;
+    // }
     int minimumCoins(vector<int>& prices) {
 
         int n=prices.size();
 
-        for(int i=0;i<=n;i++){
+        // for(int i=0;i<=n;i++){
             
-                dp[i]=-1;
+        //         dp[i]=-1;
             
+        // }
+
+        // return rec(0,prices);
+
+        vector<int>dp(n+1,1e9);
+        dp[n]=0;
+        
+
+
+        for(int i=n-1;i>=0;i--){
+            int currIdx=i+1;
+            for(int j=i+1;j<=2*currIdx;j++){
+                if(j<=n){
+                    dp[i]=min(dp[i],prices[i]+dp[j]);
+                }
+            }
         }
 
-        return rec(0,prices);
+        return dp[0];
 
         
     }
