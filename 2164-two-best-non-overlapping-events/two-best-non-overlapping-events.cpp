8904e7d5893c81150ec506b58d98fc3cbad5ba05
Vaibhav Yadav <113878147+vaibhav484848@users.noreply.github.com>
2025-12-23 13:38:17 +0530

    Time: 147 ms (39.00%) | Memory: 187.4 MB (30.12%) - LeetSync

---
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    int maxTwoEvents(vector<vector<int>>& events) {
+        // sort(events.begin(),events.end());
+        sort(events.begin(), events.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[1] < b[1];
+        });
+
+
+        int n=events.size();
+
+        vector<vector<int>>dp(n,vector<int>(3,0));
+
+        vector<int>prevMax(n,0);
+        prevMax[0]=events[0][2];
+        
+        for(int i=1;i<n;i++){
+            prevMax[i]=max(prevMax[i-1],events[i][2]);
+        }
+
+        int fScore=0;
+
+
+        dp[0][1]=events[0][2];
+
+        
+
+        for(int idx=0;idx<n;idx++){
+            int ans=-1;
+            for(int pick=1;pick<3;pick++){
+                if(pick==1)dp[idx][pick]=events[idx][2];
+
+                else{
+                    if(idx>0){
+                        int st=events[idx][0];
+                        int lo=0,hi=idx-1;
+
+                        while(lo<=hi){
+                            int mid=lo+(hi-lo)/2;
+
+                            if(events[mid][1]<st){
+                                ans=mid;
+                                lo=mid+1;
+                            }
+                            else hi=mid-1;
+
+                        }
+                        if(ans!=-1){
+                            int score=prevMax[ans];
+                            fScore=max(fScore,score+events[idx][2]);
+                        }
+                        
+                    }
+                    
+                }
+
+
+            }
+        }
+
+        return max(prevMax[n-1],fScore);
+
+    }
+};
+
+/*
+
+*/
