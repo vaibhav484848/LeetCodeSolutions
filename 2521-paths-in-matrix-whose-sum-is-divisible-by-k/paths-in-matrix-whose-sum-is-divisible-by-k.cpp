e3f5c21b20f620543b71bc0c08d75b48bf76df95
Vaibhav Yadav <113878147+vaibhav484848@users.noreply.github.com>
2025-12-23 13:07:49 +0530

    Time: 227 ms (55.64%) | Memory: 131.9 MB (66.60%) - LeetSync

--- 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
+++ 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
     const int mod=1e9+7;
-    int kk;
-    int rec(int i,int j,int rem,vector<vector<int>>& grid,vector<vector<vector<int>>>& dp){
-        if(i==0 && j==0){
-            rem+=grid[i][j];
-            rem%=kk;
-            if(rem==0) return 1;
-            return 0;
-        }
-        if(i<0 || j<0) return 0;
-
-        if(dp[i][j][rem]!=-1) return dp[i][j][rem];
-
-        int newRem=(rem+grid[i][j])%kk;
-
-        long long a=rec(i-1,j,newRem,grid,dp);
-        long long b=rec(i,j-1,newRem,grid,dp);
-
-        return dp[i][j][rem]=(a+b)%mod;
-    }
     int numberOfPaths(vector<vector<int>>& grid, int k) {
         int m=grid.size();
         int n=grid[0].size();
 
-        kk=k;
+        vector<vector<vector<int>>>dp(m,vector<vector<int>>(n,vector<int>(k,0)));
+        dp[0][0][grid[0][0]%k]=1;
 
-        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(k + 1, -1)));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) continue;
 
-        return rec(m-1,n-1,0,grid,dp);
+                for(int rem=0;rem<k;rem++){
+                    /*
+                        (prevRem+grid[i][j])%k=currRem
+                        prevRem+grid[i][j]=(currRem-grid[i][j]+k)%k
+                    */
+
+                    if(i>0){
+                        int prevRem=((rem-grid[i][j])%k+k)%k;
+                        dp[i][j][rem]=(1ll*dp[i][j][rem]+ dp[i-1][j][prevRem])%mod;
+                    }
+                    if(j>0){
+                        int prevRem=((rem-grid[i][j])%k+k)%k;
+                        dp[i][j][rem]=(1ll*dp[i][j][rem]+ dp[i][j-1][prevRem])%mod;
+                    }
+
+                } 
+            }
+        }
+
+        return dp[m-1][n-1][0];
     }
 };
